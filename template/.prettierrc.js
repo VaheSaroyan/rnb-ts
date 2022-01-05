8fc45f2de61fb2a764b1ab0915a1d6a35b05abc0
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2022-01-05 23:03:40 +0400

    init

---
+++ template/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'avoid',
+};
